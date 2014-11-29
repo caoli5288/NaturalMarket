4ab9fb53211a80c0bc6c1b1d87f0da00fe36e53a
yiwan <caoli5288@gmail.com>
2014-11-29 23:35:39 +0800

    add a adjust value to adjust final price

--- src/main/java/com/mengcraft/market/PriceTask.java
+++ src/main/java/com/mengcraft/market/PriceTask.java
@@ -1,6 +1,7 @@
 package com.mengcraft.market;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.Random;
 
@@ -24,14 +25,27 @@ public class PriceTask implements Runnable {
 		}
 		MengTable table = TableManager.getManager().getTable("NaturalMarket");
 		List<MengRecord> list = table.find("price");
+		int count = 0;
+		int sum = 1;
 		for (MengRecord record : list) {
+			count = count + 1;
+			sum = sum + Math.abs(record.getInteger("sales"));
+		}
+		BigDecimal average = new BigDecimal(sum).divide(new BigDecimal(count));
+		for (MengRecord record : list) {
+			BigDecimal adjust = new BigDecimal(record.getInteger("sales")).abs().divide(average, 2, RoundingMode.UP);
+			if (adjust.compareTo(new BigDecimal(2)) > 0) {
+				adjust = new BigDecimal(2);
+			} else if (adjust.compareTo(new BigDecimal(0.5)) < 0.5) {
+				adjust = new BigDecimal(0.5);
+			}
 			BigDecimal price = new BigDecimal(record.getString("price"));
-			BigDecimal multi = new BigDecimal(new Random().nextInt(1024)).divide(new BigDecimal("10240"));
+			BigDecimal multi = new BigDecimal(new Random().nextInt(512)).divide(new BigDecimal("10240")).multiply(adjust);
 			if (record.getInteger("sales") > 0) {
 				price = price.add(price.multiply(multi));
-			} else if (record.getInteger("sales") < 0){
+			} else if (record.getInteger("sales") < 0) {
 				price = price.subtract(price.multiply(multi));
-			} else if (new Random().nextBoolean()){
+			} else if (new Random().nextBoolean()) {
 				price = price.add(price.multiply(multi));
 			} else {
 				price = price.subtract(price.multiply(multi));
@@ -47,5 +61,4 @@ public class PriceTask implements Runnable {
 	public static PriceTask getTask() {
 		return TASK;
 	}
-
 }
